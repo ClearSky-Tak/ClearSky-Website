002329ecab28d7b542af18bc7e80d7e68ef36967
BOcort - <fixgamers01@gmail.com>
2025-10-21 09:08:08 +0900

    fix layer

--- src/main.jsx
+++ src/main.jsx
@@ -6,9 +6,9 @@ import './index.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <BrowserRouter>
+  <BrowserRouter>
+    <StrictMode>
       <App />
-    </BrowserRouter>
-  </StrictMode>,
+    </StrictMode>,
+  </BrowserRouter>
 )
